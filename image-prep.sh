3e972777bc521a5c7e87dbb645ef37202755a89d
John Deatherage <john@routeoflastresort.com>
2014-10-17 02:33:12 -0700

    VMX files plus new JSON templates - pay close attention to 14.1 changes when wrapping up

--- image-prep.sh
+++ image-prep.sh
@@ -16,22 +16,23 @@ VDISKMANAGER=vmware-vdiskmanager
 VDM_FLAGS="-c -a lsilogic -s 100GB -t0"
 # --lax is needed for 8,192MB source VM import
 OVFTOOL_FLAGS="--overwrite --acceptAllEulas --lax"
-OVAPREFIX=space-
+OVAPREFIX=space
 VERSIONS="13.3R4.4
+14.1R1.9
 "
 
 for v in $VERSIONS
 
 do
-    if test -f $DOWNLOAD_DIR/$OVA_PREFIX$v.ova;
+    if test -f $DOWNLOADDIR/$OVAPREFIX$v.ova;
     then
         echo -e "\n\n\033[32mProcessing $v:\033[0m";
         echo -e "\n\033[32mOpening $v:\033[0m";
         tar xvf $DOWNLOADDIR/$OVAPREFIX$v.ova -C $OVADIR/$v;
         echo -e "\n\033[32m[vmware-vmx]: Creating thin-provisioned 100GB disk for $v:\033[0m";
-        $VDISKMANAGER $VDM_FLAGS $VMXDIR/$v/$v-disk2.vmdk;
+        $VDISKMANAGER $VDM_FLAGS $VMXDIR/$v/$OVAPREFIX-$v-disk2.vmdk;
         echo -e "\n\033[32mPatching $v ovf:\033[0m";
-        cp -iv $FIXESDIR/$v.vmx $OVADIR/$v/;
+        cp -iv $FIXESDIR/$OVAPREFIX-$v.vmx $OVADIR/$v/;
         echo -e "\n\033[32m[vmware-vmx]: ovftool install .VMX: $VMXDIR/$v:\033[0m";
         $OVFTOOL $OVFTOOL_FLAGS $OVADIR/$v/$OVAPREFIX$v.ovf $VMXDIR/$v/$OVFPREFIX$v.vmx;
         echo -e "\n\033[32m[vmware-vmx]: patching .VMX for second disk $v:\033[0m";
